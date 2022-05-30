9350b18d74ee51501d4518b1cc8eafabe3b3ec09
dejwi <misti.krasula@gmail.com>
2022-05-30 20:21:26 +0200

    base next app

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Portfolio</title>
+        <meta name="description" content="Dawid Krasowski portfolio site" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+};
+
+export default Home;
