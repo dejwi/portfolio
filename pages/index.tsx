f5ad2a17909e4deb63571bb2f416df4cd5d52231
dejwi <misti.krasula@gmail.com>
2022-06-01 14:32:16 +0200

    added meta tags& separators

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,18 +8,39 @@ import Portfolio from "../components/projects/Portfolio/Portfolio";
 import About from "../components/About/About";
 
 const Home: NextPage = () => {
+  const tagsClass = "text-center font-poppi text-[#ADADAD] text-sm";
   return (
     <div className="bg-neutral-100 overflow-x-hidden">
       <Head>
         <title>Portfolio</title>
         <meta name="description" content="Dawid Krasowski portfolio site" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="author" content="Dawid Krasowski" />
+        <meta
+          name="keywords"
+          content="Portfolio, Dawid, Krasowski, HTML, CSS, Tailwindcss, React, Nextjs, best, pretty, minimalistic, github, developer, web"
+        />
+        <meta name="theme-color" content="#f5f5f4" />
+        <meta charSet="utf-8" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Top />
+      <p className="text-center font-poppi text-[#ADADAD] text-sm">
+        {"<projects>"}
+      </p>
       <Dejwibook />
       <PixelTogether />
       <Portfolio />
+      <p className="text-center text-[#404040] font-poppi text-[0.7rem] underline">
+        <a href="https://github.com/dejwi?tab=repositories" target="_blank">
+          older projects
+        </a>
+      </p>
+      <p className={tagsClass}>{"</projects>"}</p>
+      <div className="mt-8"></div>
+      <p className={tagsClass}>{"<about>"}</p>
       <About />
+      <p className={tagsClass}>{"</about>"}</p>
     </div>
   );
 };
