436dccf3b759bf76911a155ac9879dec0ae8b315
dejwi <misti.krasula@gmail.com>
2022-06-02 15:42:29 +0200

    added comment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import Script from "next/script";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      {/*  google analytics */}
       <Script
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
